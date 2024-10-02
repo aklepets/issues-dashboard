80ef7abd80ae8909b86dc6997e5535aafe8c711c
Andrei Klepets <ak1988.88@gmail.com>
2024-10-02 18:46:08 -0400

    actuator health endpoint improved

---
+++ src/main/java/com/example/issues_dashboard/GithubHealthIndicator.java
@@ -0,0 +1,34 @@
+package com.example.issues_dashboard;
+
+import com.example.issues_dashboard.github.GithubClient;
+import com.example.issues_dashboard.github.RepositoryEvent;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GithubHealthIndicator implements HealthIndicator {
+
+    private final GithubClient githubClient;
+
+    public GithubHealthIndicator(GithubClient githubClient) {
+        this.githubClient = githubClient;
+    }
+
+    @Override
+    public Health health() {
+        try{
+            ResponseEntity<RepositoryEvent[]> response = this.githubClient.fetchEvents("spring-projects", "spring-boot");
+            if(response.getStatusCode().is2xxSuccessful()){
+                return Health.up().build();
+            }
+            else {
+                return Health.down().build();
+            }
+        }
+        catch (Exception e){
+           return Health.down(e).build();
+        }
+    }
+}
