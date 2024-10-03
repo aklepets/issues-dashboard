783475c8ee98578ade2e091324f8e0caca573a20
Andrei Klepets <ak1988.88@gmail.com>
2024-10-02 23:55:20 -0400

    security works?

---
+++ src/main/java/com/example/issues_dashboard/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package io.spring.demo.issuesdashboard;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user")
+                        .password("{noop}password") // {noop} means no encoding
+                        .authorities("ROLE_USER")
+                        .build(),
+                User.withUsername("admin")
+                        .password("{noop}admin")
+                        .authorities("ROLE_ACTUATOR", "ROLE_ADMIN", "ROLE_USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers(EndpointRequest.to("info", "health")).permitAll()
+                        .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/events/**").hasRole("USER")
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
